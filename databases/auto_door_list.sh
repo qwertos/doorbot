d938c1c6bcd1cd422ecaee703ee68caba23173a7
Jen Herting <jen@herting.cc>
2025-10-12 13:35:48 -0400

    make out directory if it doesnt exist

--- databases/auto_door_list.sh
+++ databases/auto_door_list.sh
@@ -5,5 +5,10 @@ URL=https://${AUTHHOST}/authit/api/v1/resources/${RESOURCE}/acl
 PWFILE=/home/pi/doorbot/databases/curlpw
 OUTFILE=/home/pi/doorbot/databases/rfid/acl.json
 
+outdir="$(dirname $OUTFILE)"
+if [[ ! -d "$outdir" ]] ; then
+	mkdir -p "$outdir"
+fi
+
 curl --silent --fail --output ${OUTFILE} --netrc-file ${PWFILE} ${URL}
 
