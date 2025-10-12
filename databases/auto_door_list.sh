8dc81ca83ff9ca228f28b1754535373bc77415da
Jen Herting <jen@herting.cc>
2025-10-12 13:50:12 -0400

    using a config file

--- databases/auto_door_list.sh
+++ databases/auto_door_list.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-RESOURCE=frontdoor
-AUTHHOST=auth.makeitlabs.com
-URL=https://${AUTHHOST}/authit/api/v1/resources/${RESOURCE}/acl
-PWFILE=/home/pi/doorbot/databases/curlpw
-OUTFILE=/home/pi/doorbot/databases/rfid/acl.json
+
+. config
 
 outdir="$(dirname $OUTFILE)"
 if [[ ! -d "$outdir" ]] ; then
